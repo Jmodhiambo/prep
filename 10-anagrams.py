82728b8acadc9f032bec6d2084e6bad393ad68a7
Jmodhiambo <jmodhiambo9346@gmail.com>
2025-10-16 23:24:00 +0300

    Anagrams

---
+++ 10-anagrams.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Checks if two strings are anagrams of each other."""
+
+
+def are_anagrams(str1: str, str2: str) -> bool:
+    """
+    Checks if the two strings are anagrams of each other.
+    Args:
+        - str1 (str): Input string 1
+        - str2 (str): Input string 2
+    Return:
+        - Boolean: True is anagrams and False if otherwise
+    """
+    
